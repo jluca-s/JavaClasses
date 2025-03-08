31b5718794a02e01a92fcc1253315cf8977fe51d
Joao Lucas Goncalves <joaolucasgc@icloud.com>
2025-03-08 16:13:09 -0300

    ♻️ refactor: Melhora a apresentação do menu e formatação dos resultados na calculadora.

--- src/com/exercicios/utils/Calculadora.java
+++ src/com/exercicios/utils/Calculadora.java
@@ -41,48 +41,47 @@ public class Calculadora {
         for (double numero : numeros) {
             soma += numero;
         }
-        return soma / numeros.length;
+        return Math.round((soma / numeros.length) * 100.0) / 100.0;
     }
 
     // 3. Faça um Programa que converta metros para centímetros.
 
     public static double metrosToCentimetros(double metros) {
-        return metros * 100;
+        return Math.round((metros * 100) * 100.0) / 100.0;
     }
 
     public static double centimetrosToMetros(double centimetros) {
-        return centimetros / 100;
+        return Math.round((centimetros / 100) * 100.0) / 100.0;
     }
 
     // 4. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 
     public static double areaCirculo(double raio) {
-        return Math.PI * Math.pow(raio, 2);
+        return Math.round((Math.PI * Math.pow(raio, 2)) * 100.0) / 100.0;
     }
 
     // 5. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 
     public static double areaQuadradoDobrada(double lado) {
-
-        return (Math.pow(lado, 2)) * 2;
+        return Math.round(((Math.pow(lado, 2)) * 2) * 100.0) / 100.0;
     }
 
     // 6. Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
 
     public static double salario(double valorHora, double horasTrabalhadas) {
-        return valorHora * horasTrabalhadas;
+        return Math.round((valorHora * horasTrabalhadas) * 100.0) / 100.0;
     }
 
     // 7. Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
 
     public static double fahrenheitToCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9;
+        return Math.round(((fahrenheit - 32) * 5 / 9) * 100.0) / 100.0;
     }
     
     // 8. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
     
     public static double celsiusToFahrenheit(double celsius) {
-        return (celsius * 9 / 5) + 32;
+        return Math.round(((celsius * 9 / 5) + 32) * 100.0) / 100.0;
     }
 
     // 9. Faça um Programa que peça 2 números inteiros e um número real. Calcule e
@@ -91,11 +90,11 @@ public class Calculadora {
     // 2. a soma do triplo do primeiro com o terceiro.
     // 3. o terceiro elevado ao cubo.
 
-    public static double[] calculos(int a, int b, double c) {
-        double[] resultados = new double[3];
-        resultados[0] = (a * 2) * ((double) b / 2);
-        resultados[1] = (a * 3) + c;
-        resultados[2] = Math.pow(c, 3);
+    public static Map<String, Double> variosCalculos(int a, int b, double c) {
+        Map<String, Double> resultados = new HashMap<>();
+        resultados.put("(a * 2) * (b / 2)", Math.round(((a * 2) * ((double) b / 2)) * 100.0) / 100.0);
+        resultados.put("(a * 3) + c", Math.round(((a * 3) + c) * 100.0) / 100.0);
+        resultados.put("c ^ 3", Math.round((Math.pow(c, 3)) * 100.0) / 100.0);
         return resultados;
     }
 
@@ -103,21 +102,17 @@ public class Calculadora {
     // algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura)
     // - 58
 
-    public static double pesoIdeal(double altura) {
-        return (72.7 * altura) - 58;
-    }
-
     // 11. Tendo como dado de entrada a altura (h) de uma pessoa, construa um
     // algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
     // 1. Para homens: (72.7*h) - 58
     // 2. Para mulheres: (62.1*h) - 44.7
 
     public static double pesoIdeal(double altura, char sexo) {
-        return switch (sexo) {
+        return Math.round((switch (sexo) {
             case 'M' -> (72.7 * altura) - 58;
             case 'F' -> (62.1 * altura) - 44.7;
             default -> 0;
-        };
+        }) * 100.0) / 100.0;
     }
 
     // 12. João Papo-de-Pescador, homem de bem, comprou um microcomputador
@@ -130,13 +125,13 @@ public class Calculadora {
     // João deverá pagar. Imprima os dados do programa com as mensagens
     // adequadas.
 
-    public static Map<String, Double> excessoPeso(double peso) {
+    public static Map<String, Double> validarPesca(double peso) {
         Map<String, Double> resultado = new HashMap<>();
         if (peso > 50) {
             double excesso = peso - 50;
             double multa = excesso * 4;
-            resultado.put("excesso", excesso);
-            resultado.put("multa", multa);
+            resultado.put("excesso", Math.round(excesso * 100.0) / 100.0);
+            resultado.put("multa", Math.round(multa * 100.0) / 100.0);
         } else {
             resultado.put("excesso", 0.0);
             resultado.put("multa", 0.0);
@@ -150,13 +145,13 @@ public class Calculadora {
     // vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a
     // quantidades de latas de tinta a serem compradas e o preço total.
 
-    public static Map<String, Double> lojaTintas(double area) {
+    public static Map<String, Double> tintaNecessaria(double area) {
         Map<String, Double> resultado = new HashMap<>();
         double litros = area / 3;
         double latas = Math.ceil(litros / 18);
         double preco = latas * 80;
-        resultado.put("latas", latas);
-        resultado.put("preco", preco);
+        resultado.put("latas: ", Math.round(latas * 100.0) / 100.0);
+        resultado.put("preco: ", Math.round(preco * 100.0) / 100.0);
         return resultado;
     }
 
@@ -165,6 +160,6 @@ public class Calculadora {
     // tempo aproximado de download do arquivo usando este link (em minutos).
 
     public static double tempoDownload(double tamanho, double velocidade) {
-        return (tamanho / velocidade) / 60;
+        return Math.round(((tamanho / velocidade) / 60) * 100.0) / 100.0;
     }
 }
--- src/com/exercicios/utils/Menu.java
+++ src/com/exercicios/utils/Menu.java
@@ -4,6 +4,7 @@ public class Menu {
 
     private Menu() {
     }
+    
 
     public static void saudacao() {
         System.out.println("Bem vindo ao sistema de calculos");
@@ -13,40 +14,58 @@ public class Menu {
     
     
     public static void exibirMenu() {
+        System.out.println("-----------------------------");
         System.out.println("Escolha uma opção:");
         System.out.println("1. Operações matematicas");
         System.out.println("2. Converções");
         System.out.println("3. Calculos Corporais");
         System.out.println("4. Calculos Especificos");
-        System.out.print("Opção: ");
+        System.out.println("0. Sair");
+        System.out.println("Opção: ");
+        System.out.println("-----------------------------");
     }
-
+    
     public static void operacoesMatematicas() {
-        System.out.println("Escolha uma opção");
+        System.out.println("-----------------------------");
+        System.out.println("Operações matematicas:");
         System.out.println("1. Somar dois numeros.");
         System.out.println("2. Media aritimetica");
-        System.out.println("0. Voltar");
-                
+        System.out.println("3. Area de um circulo");
+        System.out.println("0.  Voltar");                
+        System.out.println("-----------------------------");
     }
-
+    
     public static void conversoes() {
-        System.out.println("Escolha uma opção");
+        System.out.println("-----------------------------");
+        System.out.println("Converções:");
         System.out.println("1. Converter de Celsius para Fahrenheit");
         System.out.println("2. Converter de Fahrenheit para Celsius");
         System.out.println("3. Converter de Metros para Centimetros");
         System.out.println("4. Converter de Centimetros para Metros");
-        System.out.println("0. Voltar");
+        System.out.println("0.  Voltar");
+        System.out.println("-----------------------------");
     }
     
     public static void calculosCorporais() {
-        System.out.println("Escolha uma opção");
+        System.out.println("-----------------------------");
+        System.out.println("Calculos Corporais:");
         System.out.println("1. Calcular IMC");
         System.out.println("2. Calcular Peso Ideal");
         System.out.println("0. Voltar");
+        System.out.println("-----------------------------");
     }
-
+    
     public static void calculosEspecificos() {
-        
+        System.out.println("-----------------------------");
+        System.out.println("Calculos Especificos:");
+        System.out.println("1. Realizar varios calculos");
+        System.out.println("2. Calcular area de um quadradado e mostrar o dobro");
+        System.out.println("3. Calcular salario liquido");
+        System.out.println("4. Velocidade media de download");
+        System.out.println("5. Calcular a quantidade de tinta necessaria");
+        System.out.println("6. Calcular excedente de peso de pesca");
+        System.out.println("0. Voltar");
+        System.out.println("-----------------------------");
     }
 
 
